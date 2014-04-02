b31bcbd12f7c275419f2e08043ba6826c12789ab
Dragos <dragos.cifrea@3pillarglobal.com>
2014-04-02 15:23:56 +0300

    add navigator module

---
+++ sw-front/app/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular
+  .module('swFrontApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html'
+      })
+      .when('/edges', {
+        templateUrl: 'views/edges.html',
+        controller: 'EdgesController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
