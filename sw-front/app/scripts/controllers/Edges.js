1362603cabce53dbaf66108ac9aa4c571e910e65
Dragos <dragos.cifrea@softvision.ro>
2016-02-09 16:15:14 +0200

    remove old code

--- sw-front/app/scripts/controllers/Edges.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('swFrontApp')
-    .controller('EdgesController', function () {
-        console.log('jere');
-    });
--- sw-front/app/scripts/directives/navbar.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('swFrontApp').directive('navbar', function () {
-  return {
-    restrict: 'E',
-    templateUrl: 'views/navbar.html',
-    controller: 'NavigationController'
-  };
-});
